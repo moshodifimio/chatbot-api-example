1b10fdd456625e8f57cbd277fbf47ede8064f28a
joshuaiokua <jiokua.albano@gmail.com>
2024-03-13 02:30:25 -0700

    feat: get rag info func
    Functionality to keep track of and be able to retrieve parameters that went into construction of the RAG model

--- src/rag_creator.py
+++ src/rag_creator.py
@@ -16,14 +16,27 @@ class RAGCreator():
         self.retriever = None
         self.query_engine = None
 
-    def load_documents(self, reader, kwargs:dict):
+        self.rag_info = {}
+
+    def _update_rag_info(self, params:dict):
+        params.__delitem__("self")
+        callable_obj_keys = [k for k,v in params.items() if callable(v)]
+        for k in callable_obj_keys:
+            params[k] = params[k].__name__
+        self.rag_info.update(params)
+
+    def load_documents(self, data_loader, data_loader_kwargs:dict):
+        self._update_rag_info(locals())
+        
         try:
-            docs = reader().load_data(**kwargs)
+            docs = data_loader().load_data(**data_loader_kwargs)
         except Exception as e:
             raise TypeError(f"Error loading documents: {e}.")
         self.documents = docs
 
     def parse_docs_to_nodes(self, node_parser=SimpleNodeParser, chunk_size=1024):
+        self._update_rag_info(locals())
+
         node_parser = node_parser.from_defaults(chunk_size=chunk_size)
         nodes = node_parser.get_nodes_from_documents(self.documents)
 
@@ -32,24 +45,27 @@ class RAGCreator():
         self.nodes = nodes
         
     def set_model_settings(self, open_ai_model="gpt-3.5-turbo", embed_model="local:BAAI/bge-small-en"):
+        self._update_rag_info(locals())
         load_dotenv()
         
         Settings.llm = OpenAI(model=open_ai_model)
         Settings.embed_model = resolve_embed_model(embed_model)
 
     def create_retriever(self, vector_store_impl=VectorStoreIndex, similarity_top_k=2):
+        self._update_rag_info(locals())
         index = vector_store_impl(self.nodes)
         self.retriever = index.as_retriever(similarity_top_k=similarity_top_k)
 
     def create_query_engine(self, query_engine=RetrieverQueryEngine):
+        self._update_rag_info(locals())
         self.query_engine = query_engine.from_args(self.retriever)
     
-    def setup_and_deploy_RAG(self, reader, reader_kwargs, 
+    def setup_and_deploy_RAG(self, data_loader, data_loader_kwargs, 
                              node_parser=SimpleNodeParser, chunk_size=1024, 
                              open_ai_model="gpt-3.5-turbo", embed_model="local:BAAI/bge-small-en", 
                              vector_store_impl=VectorStoreIndex, similarity_top_k=2, 
                              query_engine=RetrieverQueryEngine):
-        self.load_documents(reader, reader_kwargs)
+        self.load_documents(data_loader, data_loader_kwargs)
         self.parse_docs_to_nodes(node_parser, chunk_size)
         self.set_model_settings(open_ai_model, embed_model)
         self.create_retriever(vector_store_impl, similarity_top_k)
@@ -84,3 +100,6 @@ class RAGCreator():
         retrievals = self.fetch_relevant_info(query=query)
         for retrieval in retrievals:
             display_source_node(retrieval, source_length=source_length)
+
+    def get_rag_info(self):
+        return self.rag_info
